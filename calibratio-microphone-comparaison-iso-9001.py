3d0f8e186b29570163c87e2d4806fb3a240b3ab6
louisreine <72979526+louisreine@users.noreply.github.com>
2021-07-02 16:49:08 +0200

    changed README and added script

--- calibratio-microphone-comparaison-iso-9001.py
+++ calibratio-microphone-comparaison-iso-9001.py
@@ -1,8 +1,10 @@
-# Microphone calibration using comparison method
+'''
 Small script used to calibrate a microphone using a comparison method.
 The first microphone is considered as calibrated, and the second is considered to be calibrated.
 We use farina's method to determine the transfer function of our microphone
-Different methods will be used.
-We use Olivier Doaré's Measpy Package, which you can find [here](https://github.com/odoare/measpy).
+Different methods will be used
+'''
 
+import measpy
+import matplotlib as plt
 
