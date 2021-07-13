a3acac3871f42e2047f0c61040ad73b6baad7aff
Louis Reine <louis.reine@eleves.enpc.fr>
2021-07-13 17:52:10 +0200

    Added functionnality, the code launch a task and retrieve transfer function coefficient for every frequencies and put them in a csv file

--- calibratio-microphone-comparaison-iso-9001.py
+++ calibratio-microphone-comparaison-iso-9001.py
@@ -6,43 +6,44 @@ Different methods will be used
 Input 1 is the calibrated microhpone
 Input 2 is the uncalibrated microphone
 '''
-
+#%%
 import measpy as mp
 import matplotlib.pyplot as plt
-from measpy.audio import audio_run_measurement, audio_get_devices
+from measpy import audio
+from measpy.ni import ni_run_measurement, ni_get_devices
 plt.style.use('seaborn')
 
-indev = 'Built-in Microphone'
-outdev = 'Built-in Output'
 
+#%%
+
+#%%
 # First create a measurement
 # We send a logsweep signal to use Farina's method
-
-calibration_mesurement = mp.Measurement(out_sig='logsweep',
+# For the moment, we only run microphone measurement input, synchronised output will come later
+calibration_mesurement = mp.Measurement(out_sig=None,
                     fs = 44100,
-                    out_map=[1],
-                    out_desc=['Out1'],
-                    out_dbfs=[1.0],
                     in_map=[1,2],
                     in_desc=['Calibrated mic','Uncalibrated mic'],
                     in_cal=[1.0,1.0],
                     in_unit=['Pa','Pa'],
                     in_dbfs=[1.0,1.0],
                     extrat=[0,0],
-                    out_sig_fades=[0,0],
-                    dur=5,
-                    in_device=indev,
-                    out_device=outdev,
-                    out_amp=1.0)
+                    dur=10,
+                    in_device='myDAQ1'
+                    )
 
-audio_run_measurement(calibration_mesurement)
+print("Launching measurement !")
+ni_run_measurement(calibration_mesurement)
 
 axes_calibration = calibration_mesurement.plot()
 plt.show() #Check the data
 
-calibrated_mic_signal = calibration_mesurement.data['In1']
-uncalibrated_mic_signal = calibration_mesurement.data['In2']
+calibrated_mic_signal = calibration_mesurement.data['In1'].fft().plot()
+uncalibrated_mic_signal = calibration_mesurement.data['In2'].fft().plot()
+calibrated_mic_signal = calibration_mesurement.data['In1'].plot()
+(-calibration_mesurement.data['In1'] + calibration_mesurement.data['In2']).plot()
 
-comparison_axes = calibrated_mic_signal.tfe_farina(calibration_mesurement.out_sig_freqs).plot()
-uncalibrated_mic_signal.tfe_farina(calibration_mesurement.out_sig_freqs).plot(axes = comparison_axes)
+#comparison_axes = calibrated_mic_signal.tfe_farina(calibration_mesurement.out_sig_freqs).plot()
+#uncalibrated_mic_signal.tfe_farina(calibration_mesurement.out_sig_freqs).plot(axes = comparison_axes)
 plt.show()
+# %%
